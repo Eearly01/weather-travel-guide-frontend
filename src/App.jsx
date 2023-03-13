f6c78018efd4fa9f3aa2194881d5b36b67cd1b4f
eearly01 <elijah.early@gmail.com>
2023-03-13 16:05:38 -0500

    created repo

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// Bootstrap Components
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+// My Components
+import WeatherCard from './components/WeatherCard.jsx'
+import TravelCard from './components/TravelCard.jsx';
+
+function App() {
+  const [travelDay, setTravelDay] = useState(null);
+  const [weatherDays, setWeatherDays] = useState([]);
+  const getTravel = () => {
+    axios.get('http://localhost:3000/days').then((res) => {
+      setTravelDay(res.data)
+    })
+  }
+
+  const getWeather = () => {
+    axios.get('https://api.weather.gov/points/33.1834,-96.6318').then((res) => { axios.get(res.data.properties.forecast).then((weatherRes) => {
+      const weatherData = weatherRes.data.properties.periods;
+      setWeatherDays(weatherData);
+    })})
+  }
+
+  useEffect(() => {
+    getWeather();
+    
+  }, []);
+
+  return (
+		<Container>
+			<h1>Weather Travel Guide</h1>
+			<p>{weatherDays ? weatherDays.map((days) => {
+        return (
+          days.name
+        )
+      }) : ' '}</p>
+			<Row>
+				<WeatherCard />
+				<TravelCard />
+			</Row>
+		</Container>
+	);
+}
+
+export default App;
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,16 @@
+import {Card, ListGroup} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
+
+const WeatherCard = (props) => {
+    return (
+    <Col sm={6} md={4}>
+        <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Body>Weather Card Body</Card.Body>
+            <Card.Text>Weather Text</Card.Text>
+            {/* <Button variant='danger'>DELETE</Button> */}
+        </Card.Body>
+    </Col>);
+}
+ 
+export default WeatherCard;
