591f8911892730c7224f866cc418ad82e48414cc
eearly01 <elijah.early@gmail.com>
2023-03-14 16:58:37 -0500

    ready to style weather cards

--- src/components/DayWeather.jsx
+++ src/components/DayWeather.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import {Card} from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
@@ -10,7 +10,7 @@ function DayWeather(props) {
 	const addToWeatherCards = (e) => {
 		e.preventDefault();
 		axios.post('http://localhost:3000/days', {
-			city: props.day.name,
+			city: props.city.municipality,
 			temp: props.day.temperature,
 			windSpeed: props.day.windSpeed,
 			windDirection: props.day.windDirection,
@@ -21,28 +21,26 @@ function DayWeather(props) {
 
 	return (
 		<>
-			<Card style={{ width: '18rem' }}>
-				<Card.Body>
-					<Card.Title>{props.city.municipality}</Card.Title>
-					<Card.Subtitle className='mb-2 text-muted'>
-						{props.day.name}
-					</Card.Subtitle>
-					<Card.Text>
-						<p>
-							{props.day.temperature}
-							{props.day.temperatureUnit}
-						</p>
-						<p>{props.day.windSpeed}</p>
-						<p>{props.day.windDirection}</p>
-						<p>{props.day.detailedForecast}</p>
-					</Card.Text>
-				</Card.Body>
-			</Card>
-			<p>
-				{props.day.name}
-				{props.day.temperature}
-			</p>
-			<Button onClick={addToWeatherCards}>Add To Trip</Button>
+			<Col>
+				<Card border='Primary' style={{ width: '18rem' }}>
+					<Card.Body>
+						<Card.Title>{props.city.municipality}</Card.Title>
+						<Card.Subtitle className='mb-2 text-muted'>
+							{props.day.name}
+						</Card.Subtitle>
+						<Card.Text>
+							<p>
+								{props.day.temperature}
+								{props.day.temperatureUnit}
+							</p>
+							<p>{props.day.windSpeed}</p>
+							<p>{props.day.windDirection}</p>
+							<p>{props.day.detailedForecast}</p>
+						</Card.Text>
+					</Card.Body>
+				</Card>
+				<Button onClick={addToWeatherCards}>Add To Trip</Button>
+			</Col>
 		</>
 	);
 }
